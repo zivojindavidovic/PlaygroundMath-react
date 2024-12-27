779c0eef6ad7f2bf36989cd0f4c89f65c18e2ae0
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2024-12-27 14:42:37 +0100

    Initial react application setup

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,  // Allows access from outside the container
+    port: 5173,  // Port to expose
+  },
+})
